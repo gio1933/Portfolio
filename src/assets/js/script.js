82501dd7a9df438b05cec1eea8d6ead9fb12e97c
gio19 <gio_1933@hotmail.com>
2025-01-29 19:08:55 -0600

    Update

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -23,6 +23,7 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
+        
         //habilidades tecnicas
         habilidades[0].classList.add("typescriptjavascript");
         habilidades[1].classList.add("htmlcss");
@@ -32,21 +33,20 @@ function efectoHabilidades(){
         habilidades[5].classList.add("python");
         habilidades[6].classList.add("gitgithub");
         habilidades[7].classList.add("sql");
+        habilidades[8].classList.add("php");
 
         //habilidades profesionales
-        habilidades[8].classList.add("comunicacion");
-        habilidades[9].classList.add("trabajoequipo");
-        habilidades[10].classList.add("creatividad");
-        habilidades[11].classList.add("dedicacion");
-        habilidades[12].classList.add("puntualidad");
-        habilidades[13].classList.add("tratocliente");
-        habilidades[14].classList.add("adaptacion");
-        habilidades[15].classList.add("metodologia");
+        habilidades[9].classList.add("comunicacion");
+        habilidades[10].classList.add("trabajoequipo");
+        habilidades[11].classList.add("creatividad");
+        habilidades[12].classList.add("dedicacion");
+        habilidades[13].classList.add("puntualidad");
+        habilidades[14].classList.add("tratocliente");
+        habilidades[15].classList.add("adaptacion");
+        habilidades[16].classList.add("metodologia");
+        habilidades[17].classList.add("trellofigma");
     }
-}
-
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
+}//detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
 } 
